ca64a53caa2c96b4c02346b6eff64f704c3c51ce
nepeat@devbox <nepeat@gmail.com>
2025-05-11 11:22:45 +0000

    working nix baseline so far

---
+++ configuration.nix
@@ -0,0 +1,31 @@
+{
+  modulesPath,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.systemd-boot = {
+    enable = true;
+    consoleMode = "auto";
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+  services.openssh.enable = true;
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.htop
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMVk9i7FG7dc9r4ixwAJT7uPLH3UuqbwIgeZ7Ytmnpvv erin-laptop"
+  ];
+
+  system.stateVersion = "24.05";
+}
---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+      lix = {
+        url = "git+https://git@git.lix.systems/lix-project/lix";
+      };
+
+      lix-module = {
+        url = "git+https://git.lix.systems/lix-project/nixos-module";
+        inputs.lix.follows = "lix";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+
+      disko = {
+        url = "github:nix-community/disko";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+      nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
+  };
+
+  outputs =
+    {
+      lix-module,
+      nixpkgs,
+      disko,
+      nixos-facter-modules,
+      ...
+    }:
+    {
+      nixosConfigurations.proxmox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          lix-module.nixosModules.default
+          disko.nixosModules.disko
+          ./configuration.nix
+        ];
+      };
+
+      # Use this for all other targets
+      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
+      nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          lix-module.nixosModules.default
+          disko.nixosModules.disko
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+
+      # Slightly experimental: Like generic, but with nixos-facter (https://github.com/numtide/nixos-facter)
+      # nixos-anywhere --flake .#generic-nixos-facter --generate-hardware-config nixos-facter facter.json <hostname>
+      nixosConfigurations.generic-nixos-facter = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          lix-module.nixosModules.default
+          disko.nixosModules.disko
+          ./configuration.nix
+          nixos-facter-modules.nixosModules.facter
+          {
+            config.facter.reportPath =
+              if builtins.pathExists ./facter.json then
+                ./facter.json
+              else
+                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+          }
+        ];
+      };
+    };
+}
