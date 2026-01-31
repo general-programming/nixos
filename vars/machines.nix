45f6ba4753eb7c5031b10dcd8a4a864798d3d0bf
nepeat <nepeat@gmail.com>
2026-01-31 15:04:59 -0800

    chore: add core machine ids

--- vars/machines.nix
+++ vars/machines.nix
@@ -10,5 +10,10 @@ in
 
 nixpkgs.lib.mapAttrs (_: nixpkgs.lib.attrsets.recursiveUpdate base) {
   _ = { };
-  sea1-core = { };
+  sea1-core = {
+    machineID = "0f72ad909d2ad7732a0b7865697e89cf";
+  };
+  fmt2-core = {
+    machineID = "8b216aac9ac44002aacf262aacca1081";
+  };
 }
