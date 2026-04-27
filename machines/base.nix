79313187f523e478004b0d0d5392f801b9627c50
nepeat <nepeat@gmail.com>
2026-04-26 23:21:13 -0700

    chore: set zfs to warn

--- machines/base.nix
+++ machines/base.nix
@@ -25,7 +25,12 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+    problems.handlers = {
+      zfs.broken = "warn"; # or "ignore"
+    };
+  };
 
   # default boot settings
   boot.loader.systemd-boot = {
