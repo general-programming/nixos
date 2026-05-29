410087ee70067491f67867cc0dbdd831f6bf27c5
nepeat <nepeat@gmail.com>
2026-05-29 01:55:35 -0700

    feat: add more nfs ports for desktop

--- machines/sea420-desktop/nfs.nix
+++ machines/sea420-desktop/nfs.nix
@@ -12,7 +12,23 @@
             /srv/nfs        *(ro,fsid=root,no_subtree_check)
             /srv/nfs/consw  *(rw,no_root_squash,no_subtree_check)
         '';
+        # Pin the NFSv3 helper daemon ports so they can be firewalled.
+        lockdPort = 4001;
+        mountdPort = 4002;
+        statdPort = 4000;
     };
-    networking.firewall.allowedTCPPorts = [ 2049 ];
-    networking.firewall.allowedUDPPorts = [ 2049 ];
+    networking.firewall.allowedTCPPorts = [
+        111   # rpcbind / portmapper
+        2049  # nfsd
+        4000  # statd
+        4001  # lockd
+        4002  # mountd
+    ];
+    networking.firewall.allowedUDPPorts = [
+        111   # rpcbind / portmapper
+        2049  # nfsd
+        4000  # statd
+        4001  # lockd
+        4002  # mountd
+    ];
 }
